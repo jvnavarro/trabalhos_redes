8e7627251d2e8f98d902107cc72d596c3f1faa93
Joao Navarro <joaosd1380@gmail.com>
2025-11-11 11:37:17 -0300

    Estrutura inicial

---
+++ Stop and Wait/client_folder/client.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <fstream>
+
+int main(){
+    const char* server_ip = "127.0.0.1";
+    int server_port = 8080;
+    
+}
